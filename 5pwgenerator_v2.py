cad7669a9b6d8f30038bff9616fdbc33847ab290
aashnaregmi <regmiashna25@gmail.com>
2026-06-24 11:52:56 +0545

    feat(password-generator-v2): refactor into reusable function with improved structure

--- 5pwgenerator_v2.py
+++ 5pwgenerator_v2.py
@@ -1,29 +1,35 @@
 #updated version of pw generator i created before 
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-passwordkeys=[]
+
+def generatepw():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    passwordkeys=[]
+    nr_letters = int(input("How many letters would you like in your password?\n")) 
+  
+    nr_symbols = int(input(f"How many symbols would you like?\n"))
+    nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+    for i in range(0,nr_letters):
+        choosen_letters = random.choice(letters)
+        passwordkeys.append(choosen_letters)
+    for i in range(0,nr_symbols):
+        choosen_symbols = random.choice(symbols)
+        passwordkeys.append(choosen_symbols)
+    for i in range(0,nr_numbers):
+        choosen_numbers = random.choice(numbers)
+        passwordkeys.append(choosen_numbers)
+        
+    random.shuffle(passwordkeys)
+    newpassword="".join(passwordkeys)
+    return newpassword
+
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-for i in range(0,nr_letters):
-    choosen_letters = random.choice(letters)
-    passwordkeys.append(choosen_letters)
-for i in range(0,nr_symbols):
-    choosen_symbols = random.choice(symbols)
-    passwordkeys.append(choosen_symbols)
-for i in range(0,nr_numbers):
-    choosen_numbers = random.choice(numbers)
-    passwordkeys.append(choosen_numbers)
 
 
-random.shuffle(passwordkeys)
-newpassword="".join(passwordkeys)
-
-print(f"Your new password : {newpassword}")
+password=generatepw()
+print(f"Your new password : {password}")
 
 
