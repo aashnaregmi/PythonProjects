345813e4c25a59617d35d5b2379fcd2da8aaa2b9
aashnaregmi <regmiashna25@gmail.com>
2026-06-25 12:11:03 +0545

    implement core Higher Lower game logic with follower comparison

--- 9highestfolowers.py
+++ 9highestfolowers.py
@@ -44,5 +44,57 @@ for celebrity in celebrity_list:
     print(celebrity["location"])
     print(celebrity["followers"])
     print()
+while True:
 
+ a = random.randint(0, len(celebrity_list) - 1)
+
+ while True:
+
+    b = random.randint(0, len(celebrity_list) - 1)
+
+    if a == b:
+
+        b = random.randint(0, len(celebrity_list) - 1)
+
+    # print(a, b)
+
+
+
+    print(f"Compare A:{celebrity_list[a]["name"]},{celebrity_list[a]["work"],{celebrity_list[a]["work"]}}")
+
+    print(f"Compare B:{celebrity_list[b]["name"]},{celebrity_list[b]["work"],{celebrity_list[b]["work"]}}")
+
+
+
+    guess=input("Guess (A OR B):").lower()
+
+
+
+    max_follower=max(
+
+        (celebrity_list[a]["followers"]),
+
+        (celebrity_list[b]["followers"])
+
+    )
+
+
+
+    if guess=="a" and celebrity_list[a]["followers"]==max_follower:
+
+        print("You are correct")
+
+    elif guess == "b" and celebrity_list[b]["followers"] == max_follower:
+
+        print("You are correct")
+
+    else:
+
+        print("You are wrong")
+
+        break
+
+    a=b
+
+ break
 
