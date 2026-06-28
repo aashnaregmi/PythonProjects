bd546ae840568be64d7406da3dc2680485b32da1
aashnaregmi <regmiashna25@gmail.com>
2026-06-28 08:41:06 +0545

    start Coffee Machine project

---
+++ 12coffeemachin.py
@@ -0,0 +1,33 @@
+recources_needed={
+  "espresso":{
+         "water":50,
+         "milk":0,
+         "coffee":18,
+         "cost":1.5,
+    
+  },
+  "latte":{
+         "water":200,
+         "milk":150,
+         "coffee":24,
+         "cost":2.50,
+    
+  },
+  "capaccino":{
+         "water":250,
+         "milk":100,
+         "coffee":24,
+         "cost":3.00,
+    
+  }
+  
+}
+
+resources_available = {
+    "water": 400,   
+    "milk": 400,    
+    "coffee": 100,  
+}
+
+
+  
