d1259011af2853c610dd46b0846ffcf043700082
aashnaregmi <regmiashna25@gmail.com>
2026-07-05 20:51:23 +0545

    refactor: update quiz to use Open Trivia DB response format

--- 14-quiz-game-start/main.py
+++ 14-quiz-game-start/main.py
@@ -1,24 +1,24 @@
 from data import question_data
 from quiz_brain import QuizBrain
-class Question:
-      def __init__(self,question,ans):
-            self.que=question
-            self.ans=ans
-      
-            
-            
-            
-question_bank=[]
-for i in range (0,len(question_data)):
-    q=question_data[i]["text"]
-    a=question_data[i]["answer"]
-    question_bank.append(Question(q,a))
 
-    
-quiz=QuizBrain(question_bank)
+
+class Question:
+    def __init__(self, question, ans):
+        self.que = question
+        self.ans = ans
+
+
+question_bank = []
+for i in range(0, len(question_data)):
+    q = question_data[i]["question"]
+    a = question_data[i]["correct_answer"]
+    question_bank.append(Question(q, a))
+
+
+quiz = QuizBrain(question_bank)
 
 while quiz.still_has_question():
-      quiz.askque()
-      
-print("You completed the quiz !!")    
+    quiz.askque()
+
+print("You completed the quiz !!")
 print(f"Final score: {quiz.score}/{len(question_bank)}")
--- 14-quiz-game-start/tempCodeRunnerFile.py
+++ 14-quiz-game-start/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-from main import question_bank
+question_data
