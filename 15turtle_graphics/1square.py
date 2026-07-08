0ad2e297e5199a64cec212df679678642ea53ecb
aashnaregmi <regmiashna25@gmail.com>
2026-07-08 07:18:03 +0545

    feat: implement square drawing using turtle

--- 15turtle_graphics/1square.py
+++ 15turtle_graphics/1square.py
@@ -1,1 +1,14 @@
 # draw a square
+from turtle import Turtle, Screen
+
+screen = Screen()
+
+tim = Turtle()
+tim.color("red")
+
+
+for _ in range(4):
+    tim.left(90)
+    tim.forward(100)
+
+screen.exitonclick()
