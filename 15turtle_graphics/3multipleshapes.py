646fddf4bb759cdace10d374123434eaaf3e4fe8
aashnaregmi <regmiashna25@gmail.com>
2026-07-08 07:59:31 +0545

    feat: draw polygons using turtle graphics

--- 15turtle_graphics/3multipleshapes.py
+++ 15turtle_graphics/3multipleshapes.py
@@ -6,10 +6,12 @@ screen = Screen()
 tim = Turtle()
 tim.color("red")
 
+for i in range(3, 10):
 
-for _ in range(3):
-    tim.right(120)
-    tim.forward(100)
+    angle = int(360 / i)
+    for _ in range(i):
+        tim.forward(100)
+        tim.right(angle)
 
 
 screen.exitonclick()
