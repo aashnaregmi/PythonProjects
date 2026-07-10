6cee047dd6443d018debbd85cacaa048a60bfddf
aashnaregmi <regmiashna25@gmail.com>
2026-07-10 10:07:21 +0545

    feat: create and position multiple turtles for race

--- 17turtlerace.py
+++ 17turtlerace.py
@@ -4,6 +4,16 @@ import random
 number_of_turtle = int(input("Enter the nuber of turtle you want (2-10): "))
 
 
+turtles = []
+for i in range(number_of_turtle):
+    t = Turtle()
+    t.shape("turtle")
+    t.penup()
+    t.goto(-200, -100 + (i * 40))
+    t.pendown()
+    turtles.append(t)
+
+
 screen = Screen()
 
 
