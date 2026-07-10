e957734c43c9e6b7b746411e435cdf3b3f53eee3
aashnaregmi <regmiashna25@gmail.com>
2026-07-10 10:14:20 +0545

    feat: assign unique random colors to race turtles

--- 17turtlerace.py
+++ 17turtlerace.py
@@ -3,11 +3,28 @@ import random
 
 number_of_turtle = int(input("Enter the nuber of turtle you want (2-10): "))
 
-
+colors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "pink",
+    "brown",
+    "black",
+    "cyan",
+]
 turtles = []
+colorchoosen = []
 for i in range(number_of_turtle):
     t = Turtle()
     t.shape("turtle")
+    turtlecolor = random.choice(colors)
+    while turtlecolor in colorchoosen:
+        turtlecolor = random.choice(colors)
+    t.color(turtlecolor)
+    colorchoosen.append(turtlecolor)
     t.penup()
     t.goto(-200, -100 + (i * 40))
     t.pendown()
