1895dafc929bf9c56efd7941b97d131b891329ae
aashnaregmi <regmiashna25@gmail.com>
2026-07-11 08:22:36 +0545

    feat: hide snake creation animation using screen tracer

--- 17snakegame/main.py
+++ 17snakegame/main.py
@@ -4,17 +4,19 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)  # hide drawing animation
 
-snake_body = []
-position_of_body = [(0, 0), (-20, 0), (-40, 0)]
+bodyofsnake = []
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
 
-for i in position_of_body:
-    t = Turtle("square")
-    t.penup()
-    t.color("white")
-    t.goto(i)
-    snake_body.append(t)
+for position in starting_positions:
+    segment = Turtle("square")
+    segment.color("white")
+    segment.penup()
+    segment.goto(position)
+    bodyofsnake.append(segment)
 
+screen.update()  # show final result
 
 screen.exitonclick()
