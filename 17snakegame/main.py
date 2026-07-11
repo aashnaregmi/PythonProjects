aa541a37dbd3f99d7e89a82b810b6dce33ec88b9
aashnaregmi <regmiashna25@gmail.com>
2026-07-11 11:02:52 +0545

    feat: add Food class and integrate food into snake game

--- 17snakegame/main.py
+++ 17snakegame/main.py
@@ -9,9 +9,8 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)  # hide drawing animation
 
-
 s = Snake()
-
+f = Food()
 
 screen.listen()
 screen.onkey(fun=s.right, key="Right")
@@ -21,14 +20,19 @@ screen.onkey(fun=s.left, key="Left")
 
 
 game_on = True
-f = Food()
+
 
 f.add_food()
 while game_on:
+    head = s.bodyofsnake[0]
+    time.sleep(0.15)
 
-    time.sleep(0.15)  # Stop the program for 0.15 sec
-    s.control_snake_body()  # 3--control movement--
-    s.bodyofsnake[0].forward(20)  # 2--move--
+    s.control_snake_body()
+    s.bodyofsnake[0].forward(20)
+
+    if s.bodyofsnake[0].distance(f.food) < 15:
+        f.add_food()
+
     screen.update()
 
 screen.exitonclick()
