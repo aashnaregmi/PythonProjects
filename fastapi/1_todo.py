a212cfd68096f4fe00674a5b89ae64cff59259f3
aashnaregmi <regmiashna25@gmail.com>
2026-07-20 14:25:22 +0545

    feat: add endpoint to retrieve a todo by ID

--- fastapi/1_todo.py
+++ fastapi/1_todo.py
@@ -24,7 +24,17 @@ def create_todo(todo: Todo_data):
     return todo
 
 
-# READ
+# READ All
 @app.get("/todos")
 def get_todos():
     return todos
+
+
+# READ ONE
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return todo
+
+    return {"message": "Not found"}
